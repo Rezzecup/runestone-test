ac93ebb2ad3652569b51598e1ad4486f984c22f8
chain-wave <jacksonkaandorp_1@outlook.com>
2024-04-08 14:56:48 +0800

    Refactor flag test and add runestone test

--- test/flag.test.ts
+++ test/flag.test.ts
@@ -1,18 +1,18 @@
 import { describe, expect, it } from 'vitest'
-import { Flag, mask, set, take } from '../src/flag'
+import { Flag, maskFlag, setFlag, takeFlag } from '../src/flag'
 
 describe('flag test', () => {
   it('mask', () => {
-    expect(mask(Flag.Etch)).toEqual(0b1)
-    expect(mask(Flag.Burn)).toEqual(1 << 127)
+    expect(maskFlag(Flag.Etching)).toEqual(0b1)
+    expect(maskFlag(Flag.Cenotaph)).toEqual(1 << 127)
   })
   it('take', () => {
-    const { flags } = take(Flag.Etch, 1)
+    const { flags } = takeFlag(Flag.Etching, 1)
     expect(flags).toEqual(0)
-    const { flags: f } = take(Flag.Etch, 0)
+    const { flags: f } = takeFlag(Flag.Etching, 0)
     expect(f).toEqual(0)
   })
   it('set', () => {
-    expect(set(Flag.Etch, 0)).toEqual(1)
+    expect(setFlag(Flag.Etching, 0)).toEqual(1)
   })
 })
